ef4c291fc88b512639b2a551f320ec14a0969802
Steven Hunt <steven.mtw.hunt@gmail.com>
2016-07-22 16:17:48 -0400

    Initial build completed, updated readme.

--- README.md
+++ README.md
@@ -1,2 +1,88 @@
-# lobo-irl
-Queries live data from the Land/Ocean Biogeochemical Observatory sensors operating in the Indian River Lagoon.
+# Land/Ocean Biogeochemical Observatory - Indian River Lagoon
+Queries live data from the Lobo sensors operating in the Indian River Lagoon.
+
+For more information about the sensors, visit http://fau.loboviz.com.
+## Installation
+
+node:
+
+```
+$ npm install lobo-irl
+```
+
+## Usage
+
+Retrieving a list of available sensors.
+
+```js
+var api = require('lobo-irl');
+console.log(api.getSensors());
+```
+
+Retrieving a list of available measurements.
+
+```js
+var api = require('lobo-irl');
+console.log(api.getMeasurements());
+```
+
+Retrieving information about a specific sensor.
+
+```js
+var api = require('lobo-irl');
+console.log(api.getSensor('IRL-JB'));
+```
+
+Retrieving information about a specific measurement.
+
+```js
+var api = require('lobo-irl');
+console.log(api.getMeasurement('temperature'));
+```
+
+Query live data from a specific sensor.
+
+```js
+var api = require('lobo-irl');
+api.getSensorData('IRL-JB', function (err, result) {
+    console.log(result);
+});
+```
+
+## Sensors
+The following sensors are available to query.
+
+- **IRL-FP** - Indian River Lagoon - Fort Pierce
+- **IRL-LP** - Indian River Lagoon - Link Port
+- **IRL-SB** - Indian River Lagoon - Sebastian
+- **IRL-VB** - Indian River Lagoon - Vero Beach
+- **IRL-JB** - Indian River Lagoon-Jensen Beach
+- **IRL-SLE** - Indian River Lagoon-St. Lucie Estuary
+- **SLE-ME** - St. Lucie Estuary-Middle Estuary
+- **SLE-NF** - St. Lucie Estuary-North Fork
+- **SLE-SF** - St. Lucie Estuary-South Fork
+
+## Measurements
+The following measurement data is available.
+
+- temperature
+- salinity
+- dissolved oxygen
+- oxygen saturation
+- turbidity
+- cdom
+- chlorophyll
+- nitrate
+- pH
+- phosphate
+- depth
+- current direction
+- current speed
+- humidity
+- par
+- barometric pressure
+- rain
+- air temperature
+- wind direction
+- wind speed
+- wind gust 
--- index.js
+++ index.js
@@ -1,84 +1,37 @@
+/*
+ * Lobo - Indian River Lagoon Client
+ * Written by Steven Hunt
+ * MIT License
+ */
 
-var request = require('superagent'),
+var config  = require('config'),
+    request = require('superagent'),
     xml2js  = require('xml2js'),
     _       = require('lodash');
 
-/**
- * @private
- * Lists all available measurement types and their information.
- * All data sourced from http://fau.loboviz.com
- */
-var measurements = {
-    temperature: {  },
-    salinity: { }
-};
+// hash table for getting the data name.
+var measurementNames = {};
 
-/**
- * @private
- * Lists all sensors in the network and their information.
- * All data sourced from http://fau.loboviz.com
- */
-var sensors = {
-    'IRL-FP': {
-        url: 'http://fau.loboviz.com/0054.wml',
-        description: 'Indian River Lagoon - Fort Pierce',
-        location: { lat: 27.475555, lng: -80.326607 }
-    },
-    'IRL-LP': {
-        url: 'http://fau.loboviz.com/0035.wml',
-        description: 'Indian River Lagoon - Link Port',
-        location: { lat: 27.534839, lng: -80.343113 }
-    },
-    'IRL-SB': {
-        url: 'http://fau.loboviz.com/0056.wml',
-        description: 'Indian River Lagoon - Sebastian',
-        location: { lat: 27.839089, lng: -80.470822 }
-    },
-    'IRL-VB': {
-        url: 'http://fau.loboviz.com/0055.wml',
-        description: 'Indian River Lagoon - Vero Beach',
-        location: { lat: 27.590117, lng: -80.355258 }
-    },
-    'IRL-JB': {
-        url: 'http://fau.loboviz.com/0062.wml',
-        description: 'Indian River Lagoon-Jensen Beach',
-        location: { lat: 27.224389, lng: -80.202333 }
-    },
-    'IRL-SLE': {
-        url: 'http://fau.loboviz.com/0061.wml',
-        description: 'Indian River Lagoon-St. Lucie Estuary',
-        location: { lat: 27.164947, lng: -80.171844 }
-    },
-    'SLE-ME': {
-        url: 'http://fau.loboviz.com/0060.wml',
-        description: 'St. Lucie Estuary-Middle Estuary',
-        location: { lat: 27.208928, lng: -80.247974 }
-    },
-    'SLE-NF': {
-        url: 'http://fau.loboviz.com/0058.wml',
-        description: 'St. Lucie Estuary-North Fork',
-        location: { lat: 27.210028, lng: -80.269200 }
-    },
-    'SLE-SF': {
-        url: 'http://fau.loboviz.com/0057.wml',
-        description: 'St. Lucie Estuary-South Fork',
-        location: { lat: 27.187789, lng: -80.264180 }
-    }
-};
+// build the hash table.
+_.keys(config.measurements).forEach(function (key) {
+    measurementNames[key] = config.measurements[key].name;
+});
 
 /**
  * Returns a list of sensors.
- * @param minLat (Optional) The minimum latitude.
- * @param maxLat (Optional) The maximum latitude.
- * @param minLng (Optional) The minimum longitude.
- * @param maxLng (Optional) The maximum longitude.
+ * @param [minLat] {float} The minimum latitude.
+ * @param [maxLat] {float} The maximum latitude.
+ * @param [minLng] {float} The minimum longitude.
+ * @param [maxLng] {float} The maximum longitude.
  * @returns {Array} The collection of sensor names within the given area.
  */
 exports.getSensors = function (minLat, maxLat, minLng, maxLng) {
+
+    // if a geographic box is defined, limit by lat/lon bounds.
     if (minLat && maxLat && minLng && maxLng) {
         var result = [];
-        for (var sensor in sensors) {
-            if (sensors.hasOwnProperty(sensor)) {
+        for (var sensor in config.sensors) {
+            if (config.sensors.hasOwnProperty(sensor)) {
                 if (sensor.location.lat >= minLat &&
                     sensor.location.lat <= maxLat &&
                     sensor.location.lng >= minLng &&
@@ -89,12 +42,130 @@ exports.getSensors = function (minLat, maxLat, minLng, maxLng) {
         }
         return result;
     }
-    return _(sensors).keys();
+
+    // otherwise, just return the full list of keys.
+    return _.keys(config.sensors);
 };
 
-exports.getSensors = function (minLat, maxLat, minLng, maxLng) {
+/**
+ * Gets detailed information about a specific sensor.
+ * @param key {string} The sensor name.
+ * @returns {Object} Information about the sensor.
+ */
+exports.getSensor = function (key) {
+    return config.sensors[key];
 };
 
-exports.getSensorInfo = function (sensor) {
-    return sensors[sensor];
+/**
+ * @private
+ * Processes the data for the given sensor.
+ * @param sensor {string} The sensor key.
+ * @param text {string} The raw sensor data.
+ * @param callback {function(err,result)} A callback.
+ */
+function processSensorData(sensor, text, callback) {
+
+    var resultLines = [],
+        resultMeasurements = [],
+        result = {},
+        time = null,
+        measurements = exports.getMeasurements();
+
+    // split the result by line
+    text.split("\n").forEach(function (line) {
+
+        // example:
+        // <p>2016-07-22 09:00:00 EST</p>
+        if (line.indexOf('<p>') === 0 && line.indexOf(':') > 0) {
+            time = line
+                .replace('/p', 'p')
+                .replace(/<p>/g, '');
+        }
+        else {
+            // for each line look for the measurement name at the beginning of the line.
+            measurements.every(function (key) {
+                if (line.indexOf(measurementNames[key] + ": ") === 0) {
+                    resultLines.push(line);
+                    resultMeasurements.push(key);
+                    return false; // stop looping if we found the match.
+                }
+                return true;
+            });
+        }
+    });
+
+    // Example line:
+    // pH: <b>7.947</b> <br/>
+    for (var i = 0; i < resultLines.length; i++) {
+
+        // extract the measurement value.
+        result[resultMeasurements[i]] = resultLines[i]
+            .replace('<b>', '') // remove the beginning <b> tag.
+            .split('</b>')[0] // take everything to the left of the ending <b> tag.
+            .split(':')[1] // split by the colon and take the right value.
+            .trim();
+
+        // if the measurement is a float, parse it.
+        if (exports.getMeasurement(resultMeasurements[i]).type === 'float') {
+            result[resultMeasurements[i]] = parseFloat(result[resultMeasurements[i]]);
+        }
+    }
+
+    // report back on the data.
+    callback(null, {
+        sensor: sensor,
+        dateTime: time,
+        data: result
+    });
+}
+
+/**
+ * Queries the given sensor for the latest measurements.
+ * @param key {string} The sensor key.
+ * @param callback {function(err, result)} A callback.
+ */
+exports.getSensorData = function (sensor, callback) {
+
+    var sensorInfo = exports.getSensor(sensor);
+
+    request
+        .get(sensorInfo.url)
+        .send()
+        .end(function(err, res) {
+
+            if (err) {
+                callback(err);
+                return;
+            }
+
+            try {
+                processSensorData(sensor, res.text, callback);
+            }
+            catch (err) {
+                callback(err);
+            }
+        });
 };
+
+/**
+ * Gets a list of supported measurements.
+ * @returns {Array} A list of measurements.
+ */
+exports.getMeasurements = function () {
+    return _.keys(config.measurements);
+};
+
+/**
+ * Gets detailed information about a specific measurement.
+ * @param key The measurement name.
+ * @returns {Object} Information about the measurement.
+ */
+exports.getMeasurement = function (key) {
+    return config.measurements[key];
+};
+
+/*exports.getSensorData('IRL-JB', function (err, result) {
+    console.log(result);
+});*/
+
+console.log(exports.getMeasurements());
